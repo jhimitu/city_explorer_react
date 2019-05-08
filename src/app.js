57b4d40db917f652191060104249bd6899e3f3c0
Jhia Turner <jhimitu@gmail.com>
2019-05-08 09:40:49 -0700

    create Header component

--- src/app.js
+++ src/app.js
@@ -3,11 +3,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class Header extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>City Explorer</h1>
+        <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
+      </React.Fragment>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World!</h1>
+        <Header />
       </React.Fragment>
     );
   }
