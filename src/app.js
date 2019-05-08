0fd68153838505fa59df7659985bcff9b0c6ce94
Jhia Turner <jhimitu@gmail.com>
2019-05-08 09:34:04 -0700

    create App component

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World!</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 'use strict';
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
+
+const app = document.getElementById('app');
+ReactDOM.render(<App/>, app);
