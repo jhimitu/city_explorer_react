a4615f6d9d3166262a80fc4f16979b31d0969683
Jhia Turner <jhimitu@gmail.com>
2019-05-08 21:31:49 -0700

    put header component in its own file and export to app

--- src/app.js
+++ src/app.js
@@ -1,24 +1,14 @@
 'use strict';
 
 import React from "react";
+import Header from "./header.js";
 import SearchForm from "./search-form.js";
-
-class Header extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <h1>City Explorer</h1>
-        <p>Enter a location below to learn about the weather, events, restaurants, movies filmed there, and more!</p>
-      </React.Fragment>
-    );
-  }
-}
+import SearchResults from "./search-result.js";
 
 class Main extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <SearchForm />
         <Map />
         <Result />
         <Result />
@@ -67,7 +57,9 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <Header />
+        <SearchForm />
         <Main />
+        <SearchResults />
       </React.Fragment>
     );
   }
--- src/search-result.js
+++ src/search-result.js
@@ -1,1 +1,18 @@
 'use strict';
+
+import React from "react";
+
+class SearchResults extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <div>placeholder for results</div>
+      </>
+    );
+  }
+}
+
+export default SearchResults;
