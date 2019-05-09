ff3c5ff60128835b30d16b0736f1292c0509af5a
Jhia Turner <jhimitu@gmail.com>
2019-05-09 12:59:40 -0700

    implement search-form component to render for appropriate state values

--- src/components/search-form.js
+++ src/components/search-form.js
@@ -6,15 +6,43 @@ class SearchForm extends React.Component {
   constructor(props) {
     super(props);
   }
-  
+
   render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="search">Search for a location</label>
-        <input type="text" name="search" placeholder="Enter a location here" />
-        <button>Explore!</button>
-      </React.Fragment>
-    );
+    console.log(this.props.backendUrl, this.props.googleApi);
+    if (this.props.backendUrl && this.props.googleApi) {
+      return (
+        <React.Fragment>
+          <form onSubmit={this.props.handleSubmit}>
+            <label htmlFor="search">Search for a location</label>
+            <input type="text" name="search" placeholder="Enter a location here" onChange={this.props.handleCityChange} />
+            <button>Explore!</button>
+          </form>
+        </React.Fragment>
+      );
+    } else if (this.props.backendUrl) {
+      return (
+        <React.Fragment>
+            <form id="geocode-form" onSubmit={this.props.handleSubmit}>
+              <label>Enter your Geocoding API Key:</label>
+              <input type="text" id="api-key" onChange={this.props.handleApiChange} />
+            </form>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <React.Fragment>
+          <form onSubmit={this.props.handleSubmit}>
+            <label>Enter the URL to your deployed back end, making sure to remove the trailing forward slash</label>
+            <input type="text" id="back-end-url" onChange={this.props.handleUrlChange} />
+          </form>
+
+          <form id="geocode-form" onSubmit={this.props.handleSubmit}>
+            <label>Enter your Geocoding API Key:</label>
+            <input type="text" id="api-key" onChange={this.props.handleApiChange} />
+          </form>
+        </React.Fragment>
+      );
+    }
   }
 }
 
